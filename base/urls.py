801e18cc034d27c061817e0d68542617e78c3c18
Rudaba-Kamor <rudabaadninkamor@gmail.com>
2023-07-18 21:49:37 +0600

    store

--- base/urls.py
+++ base/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns= [
     path('', views.home, name = 'home'),
+    path('blog/', views.blog, name = 'blog'),
+    path('contact/', views.contact, name = 'contact'),
+    path('shoppingcart/', views.shoppingcart, name = 'shoppingcart'),
+    path('dashboard/', views.dashboard, name = 'dashborad'),
+    path('login/', views.login, name = 'login'),
 ]
--- base/views.py
+++ base/views.py
@@ -4,3 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render (request, 'home.html')
+def blog(request):
+    return render (request, 'blog.html')
+def contact(request):
+    return render (request, 'contact.html')
+def shoppingcart(request):
+    return render (request, 'shoppingcart.html')
+def dashboard(request):
+    return render (request, 'dashboard.html')
+def login(request):
+    return render (request, 'login.html')
